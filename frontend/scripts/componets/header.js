b6352403b6a6bb9a6ab0e4aa30f7bdca2b89f8a8
DMontoyaHolgado <120228094+daivid04@users.noreply.github.com>
2025-10-07 09:50:54 -0500

    Feature: feature/Base Modulo Inventario (#9)
    * feat:Pantalla flotante para agregar/editar
    
    * style:Actualizar maquetado de agregar/editar
    
    * feat:Maquetado de pantalla confirmacion
    
    * feat:Creacion conexion a la API
    
    * feature: Creacion de componente side-bar
    
    * docs:Eliminar comentarios irrelevantes
    
    * feature:Componente header creado
    
    * feat: adaptabilidad del módulo de inventario
    
    * feat:Adaptabilidad de side bar
    
    * Feat: Estilo de los modals
    
    * feat:Creacion del inventario Data local
    
    * feat:Creacion del crud
    
    * feat:Conexion con la bd superbase
    
    * feat:Adaptar modals y para movil

---
+++ frontend/scripts/componets/header.js
@@ -0,0 +1,10 @@
+export function loadHeader(param) {
+    return `
+    <header id="header">
+        <div class="header-container">
+            <img src="../assets/images/logo.svg" alt="Logo Taller Diego" class="logo">
+            <h1>Taller Diego</h1>
+        </div>
+    </header>
+    `;
+}
