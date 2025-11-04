a531ea3e787f0cdf6807bd645503f94d61894e89
Alex Yasmani Huaracha Bellido <ayhuarachab@unjbg.edu.pe>
2025-11-03 23:10:40 -0500

    modificaion de index y documentacion

--- frontend/scripts/componets/side_bar.js
+++ frontend/scripts/componets/side_bar.js
@@ -1,39 +1,52 @@
-export function loadSideBar(){
+/**
+ * Carga y retorna el HTML del menú lateral (sidebar)
+ * Este componente muestra la navegación principal de la aplicación
+ * @returns {string} HTML del sidebar con todos los enlaces de navegación
+ */
+export function loadSideBar() {
     return `
-        <aside id="sidebar" class="side-bar-container">
-            <nav class="sidebar-nav">
-                <ul>
-                    <li>
-                        <a href="#">
-                            <img src="../assets/icons/home.png" alt="Home" class="icon">
-                            <span>Inicio</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <img src="../assets/icons/package.png" alt="Inventario" class="icon">
-                            <span>Inventario</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <img src="../assets/icons/toolbox.png" alt="Servicios" class="icon">
-                            <span>Servicios</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <img src="../assets/icons/checklist.png" alt="Orden de servicio" class="icon">
-                            <span>Orden</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <img src="../assets/icons/leader.png" alt="Personal" class="icon">
-                            <span>Personal</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </aside>`;
+        <nav class="sidebar-nav">
+            <ul>
+                <!-- Opción de navegación: Inicio/Dashboard -->
+                <li>
+                    <a href="index.html">
+                        <img src="../assets/icons/home.png" alt="Home" class="icon">
+                        <span>Inicio</span>
+                    </a>
+                </li>
+                
+                <!-- Opción de navegación: Inventario -->
+                <li>
+                    <a href="inventary.html">
+                        <img src="../assets/icons/package.png" alt="Inventario" class="icon">
+                        <span>Inventario</span>
+                    </a>
+                </li>
+                
+                <!-- Opción de navegación: Servicios -->
+                <li>
+                    <a href="#">
+                        <img src="../assets/icons/toolbox.png" alt="Servicios" class="icon">
+                        <span>Servicios</span>
+                    </a>
+                </li>
+                
+                <!-- Opción de navegación: Órdenes de servicio -->
+                <li>
+                    <a href="#">
+                        <img src="../assets/icons/checklist.png" alt="Orden de servicio" class="icon">
+                        <span>Orden</span>
+                    </a>
+                </li>
+                
+                <!-- Opción de navegación: Personal -->
+                <li>
+                    <a href="#">
+                        <img src="../assets/icons/leader.png" alt="Personal" class="icon">
+                        <span>Personal</span>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    `;
 }
