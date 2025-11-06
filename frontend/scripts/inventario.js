5b5babd135cd7321631ff7d8f5519c79f7dc51a1
daivid04 <davidmontoya210204@gmail.com>
2025-11-05 21:09:11 -0500

    refactor: reestrucurar archivos del modulo inventario

--- frontend/scripts/inventario.js
+++
@@ -1,9 +0,0 @@
-import { getProductos, getAutopartes } from './api_service.js';
-
-document.addEventListener('DOMContentLoaded', async () => {
-    const listaProductos = await getProductos();
-    console.log("Productos cargados:", listaProductos);
-
-    const listaAutopartes = await getAutopartes();
-    console.log("Autopartes cargadas:", listaAutopartes);
-});
