9f37fa641f59df935c7938c442c72e521669cec3
daivid04 <davidmontoya210204@gmail.com>
2025-11-06 23:31:00 -0500

    feat: validaciones de los datos de entrada

--- frontend/scripts/componets/modal-add-product.js
+++ frontend/scripts/componets/modal-add-product.js
@@ -214,7 +214,54 @@ export function setupModalEvents() {
       formData.anio = form['product-year'].value.trim();
     }
 
+    //VALIDACIONES
+    let isValid = true;
     
+    if (!formData.nombre) {
+      validateField(form['product-name'], "El nombre es obligatorio");
+      isValid = false;
+    }
+
+    if (!formData.marca) {
+      validateField(form['product-brand'], "La marca es obligatoria");
+      isValid = false;
+    }
+
+    if (!formData.categoria) {
+      validateField(form['product-category'], "Selecciona una categoría");
+      isValid = false;
+    }
+
+    if (formData.stock < 0) {
+      validateField(form['product-stock'], "El stock debe ser un número válido");
+      isValid = false;
+    }
+
+    if (formData.precioVenta <= 0) {
+      validateField(form['product-selling-price'], "El precio debe ser un número positivo");
+      isValid = false;
+    }
+
+    if (formData.precioVenta <= formData.precioCompra) {
+      validateField(form['product-selling-price'], "El precio de venta debe ser mayor al de compra");
+      isValid = false;
+    }
+
+    if(isAutopart) {
+      if(!formData.modelo) {
+        validateField(form['product-model'], "Debe colocar modelos");
+        isValid = false;
+      }
+      if(!formData.anio) {
+        validateField(form['product-year'], "Coloque los años de los modelos");
+        isValid = false;
+      }
+    }
+
+    if (!isValid) {
+      return;
+    }
+
     // ENVIO DE DATOS
     try {
       const endpoint = isAutopart ? 'autopartes' : 'productos';
