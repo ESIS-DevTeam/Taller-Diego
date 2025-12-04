c2c57b1920293ade34bb275b859bfacd615141ae
daivid04 <davidmontoya210204@gmail.com>
2025-12-04 00:33:18 -0500

    feat: Permisos al entrar a otras vistas

--- frontend/scripts/login.js
+++ frontend/scripts/login.js
@@ -5,6 +5,7 @@ function loginActions() {
   const form = document.querySelector(".form-login");
   const userInput = document.getElementById("user-email");
   const passwordInput = document.getElementById("user-key");
+  const errorMsg = document.getElementById('errorMsg');
 
   
   form.addEventListener("submit", async (e) => {
@@ -12,14 +13,12 @@ function loginActions() {
     const data = await obtainToken(userInput.value, passwordInput.value);
     
     if(!data) {
-      console.error('Intente de nuevo');
+      errorMsg.classList.remove("hidden");
     }else{
+      errorMsg.classList.add("hidden");
       const token = data.session.access_token;
       const user = data.user;
-      console.log(`Token: ${token}`);
-      console.log(`Usuario: ${user.email}`);
       localStorage.setItem('supabase_token', token);
-      localStorage.setItem('user_email', user.email);
       window.location.href = "../views/index.html";
     }
 
